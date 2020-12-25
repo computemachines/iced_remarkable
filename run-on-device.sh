6ab8fb10d741e934177e28fad7b46985a3446010
Tyler Parker <tparker@computemachines.com>
2020-12-25 13:58:20 -0500

    Prevent conflict between xochitl and tests.

--- run-on-device.sh
+++ run-on-device.sh
@@ -3,13 +3,13 @@
 function trap_ctrlc ()
 {
     echo "Caught Ctrl-C ..."
-#    ssh rm systemctl start xochitl
+   ssh rm systemctl start xochitl
 }
 
 trap "trap_ctrlc" SIGINT
 
-#ssh rm killall counter && echo "killed existing process"
-#ssh rm systemctl stop xochitl
+ssh rm killall counter && echo "killed existing process"
+ssh rm systemctl stop xochitl
 ssh rm ./counter && echo "Closed normally"
-#echo "Starting xochitl ..."
-#ssh rm systemctl start xochitl
+echo "Starting xochitl ..."
+ssh rm systemctl start xochitl
