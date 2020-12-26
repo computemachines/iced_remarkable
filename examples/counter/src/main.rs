f16c71ca31d7f96b58899d50af6eddbdd1c08176
Tyler Parker <tparker@computemachines.com>
2020-12-26 15:08:40 -0500

    Replace my renderer with iced_graphics renderer.

--- examples/counter/src/main.rs
+++ examples/counter/src/main.rs
@@ -1,10 +1,10 @@
 #![feature(nll)]
+use iced_graphics::Renderer;
 use iced_native::{
-    input::mouse,
     widget::{button, Button, Column, Text},
     Cache, Element, Event, Size, UserInterface,
 };
-use iced_remarkable::RemarkableRenderer;
+use iced_remarkable::backend::RemarkableBackend;
 use libremarkable::framebuffer::{
     common::{DISPLAYHEIGHT, DISPLAYWIDTH},
     core::Framebuffer,
@@ -28,7 +28,7 @@ impl Counter {
     pub fn new() -> Self {
         Counter::default()
     }
-    pub fn view(&mut self) -> Text {
+    pub fn view(&self) -> Text<Renderer<RemarkableBackend>> {
         // We use a column: a simple vertical layout
         // Column::new()
         //     .push(
@@ -47,7 +47,7 @@ impl Counter {
         //         Button::new(&mut self.decrement_button, Text::new("-"))
         //             .on_press(Message::DecrementPressed),
         //     )
-        Text::new(&self.value.to_string())
+        Text::new(&self.value.to_string()).size(10)
     }
     pub fn update(&mut self, message: Message) {
         match message {
@@ -63,18 +63,21 @@ impl Counter {
 fn main() {
     let mut counter = Counter::new();
     let mut cache = Cache::new();
-    let mut renderer = RemarkableRenderer::new();
+    let mut backend = RemarkableBackend::new();
+    let mut renderer = Renderer::new(backend);
     let mut window_size = Size::new(DISPLAYWIDTH as f32, DISPLAYHEIGHT as f32);
     let mut i = 0;
     loop {
-        let user_interface: UserInterface<Message, RemarkableRenderer> =
+        println!("loop {}", i);
+        let mut user_interface: UserInterface<Message, Renderer<RemarkableBackend>> =
             UserInterface::build(counter.view(), window_size, cache, &mut renderer);
         // let messages = user_interface.update(
         //     vec![Event::Mouse(mouse::Event::CursorMoved { x: 40.0, y: 40.0 })],
         //     None,
         //     &renderer,
         // );
-        // let primitive = user_interface.draw(&mut renderer);
+        let primitive = user_interface.draw(&mut renderer, iced_graphics::Point { x: 0.0, y: 0.0 });
+        renderer.backend_mut().render(&primitive.0);
         cache = user_interface.into_cache();
         // renderer.render(&primitive);
         i = i + 1;
---
+++ src/backend.rs
@@ -0,0 +1,78 @@
+use iced_graphics::backend::{Backend, Text};
+use iced_graphics::Primitive;
+
+use libremarkable::framebuffer::{
+    common::{color, display_temp, dither_mode, mxcfb_rect, waveform_mode},
+    core::Framebuffer,
+    refresh::PartialRefreshMode,
+    FramebufferBase, FramebufferRefresh,
+};
+
+pub struct RemarkableBackend<'a> {
+    framebuffer: Framebuffer<'a>,
+}
+
+impl Backend for RemarkableBackend<'_> {
+    fn trim_measurements(&mut self) {
+        println!("trim measurement cache?",);
+    }
+}
+
+impl Text for RemarkableBackend<'_> {
+    const ICON_FONT: iced_graphics::Font = iced_graphics::Font::Default;
+
+    const CHECKMARK_ICON: char = 'X';
+
+    const ARROW_DOWN_ICON: char = 'V';
+
+    fn default_size(&self) -> u16 {
+        60
+    }
+
+    fn measure(
+        &self,
+        contents: &str,
+        size: f32,
+        font: iced_graphics::Font,
+        bounds: iced_graphics::Size,
+    ) -> (f32, f32) {
+        (contents.len() as f32 * size, size)
+    }
+}
+
+impl Default for RemarkableBackend<'_> {
+    fn default() -> Self {
+        Self {
+            framebuffer: Framebuffer::new("/dev/fb0"),
+        }
+    }
+}
+
+impl RemarkableBackend<'_> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn render(&mut self, primitive: &Primitive) {
+        dbg!(primitive);
+    }
+    pub fn update_full(&self) {
+        self.framebuffer.full_refresh(
+            waveform_mode::WAVEFORM_MODE_GC16,
+            display_temp::TEMP_USE_REMARKABLE_DRAW,
+            dither_mode::EPDC_FLAG_USE_REMARKABLE_DITHER,
+            0,
+            true,
+        );
+    }
+    pub fn update_partial(&self, region: &mxcfb_rect) {
+        self.framebuffer.partial_refresh(
+            region,
+            PartialRefreshMode::Async,
+            waveform_mode::WAVEFORM_MODE_GLR16,
+            display_temp::TEMP_USE_REMARKABLE_DRAW,
+            dither_mode::EPDC_FLAG_USE_DITHERING_PASSTHROUGH,
+            0,
+            false,
+        );
+    }
+}
